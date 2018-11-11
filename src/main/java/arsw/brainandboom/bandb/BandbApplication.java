38062e6e5298377df0dba6c53df3e9f1db8b22e1
JuanJoseAndrade <juan.andrade96@hotmail.com>
2018-11-11 13:34:33 -0500

    conexión f y log

---
+++ src/main/java/arsw/brainandboom/bandb/BandbApplication.java
@@ -0,0 +1,12 @@
+package arsw.brainandboom.bandb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandbApplication.class, args);
+	}
+}
---
+++ src/main/java/arsw/brainandboom/restcontroller/BrainAndBoomController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package arsw.brainandboom.restcontroller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author Brain&Boom Team
+ */
+
+
+public class BrainAndBoomController { 
+}
